a0a145221df548c74b1fedcfb3c35dfaa2f3f400
Jean Souza <jeancsouzak@gmail.com>
2020-06-25 03:35:28 -0300

    Fixing healing runes and healing spells formula

--- data/spells/scripts/runes/ultimate healing.lua
+++ data/spells/scripts/runes/ultimate healing.lua
@@ -2,7 +2,7 @@ local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_HEALING)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 combat:setParameter(COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
-combat:setParameter(COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
+-- combat:setParameter(COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 
 function onGetFormulaValues(player, level, maglevel)
@@ -12,5 +12,6 @@ end
 combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(creature, variant, isHotkey)
+	doRemoveCondition(creature, CONDITION_PARALYZE) 	
 	return combat:execute(creature, variant)
 end
